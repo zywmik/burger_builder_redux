bf96012768e78990e913bf5e2c5164a31673bf1d
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-01-09 11:59:12 +0100

    React.lazy and some react hooks exercises

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -6,17 +6,16 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
     return import("./containers/Checkout/Checkout")
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
     return import("./containers/Auth/Auth")
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
     return import("./containers/Orders/Orders")
 });
 
@@ -28,7 +27,7 @@ class App extends Component {
     render () {
         let routes = (
             <Switch>
-                <Route path="/auth" component={asyncAuth} />
+                <Route path="/auth" render={(props) => <Auth {...props} />} />
                 <Route path="/" exact component={BurgerBuilder} />
                 <Redirect to="/" />
             </Switch>
@@ -37,10 +36,10 @@ class App extends Component {
         if (this.props.isAuthenticated) {
             routes = (
                 <Switch>
-                    <Route path="/checkout" component={asyncCheckout} />
-                    <Route path="/orders" component={asyncOrders} />
+                    <Route path="/checkout" render={(props) => <Checkout {...props} />} />
+                    <Route path="/orders" render={(props) => <Orders {...props} />} />
                     <Route path="/logout" component={Logout} />
-                    <Route path="/auth" component={asyncAuth} />
+                    <Route path="/auth" render={(props) => <Checkout {...props} />} />
                     <Route path="/" exact component={BurgerBuilder} />
                     <Redirect to="/" />
                 </Switch>
@@ -50,7 +49,9 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    {routes}
+                    <Suspense fallback={<p>Loading...</p>}>
+                        {routes}
+                    </Suspense>
                 </Layout>
             </div>
         );
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,32 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
+const modal = (props) => {
 
-    shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-
-    componentWillUpdate () {
-    }
-
-    render () {
-        return (
-            <React.Fragment>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+            <Backdrop show={props.show} clicked={props.modalClosed} />
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>
+        </React.Fragment>
+    );
 }
 
-export default Modal;
+
+export default React.memo(modal, (prevProps, nextProps) => prevProps.show === nextProps.show && prevProps.children === nextProps.children);
